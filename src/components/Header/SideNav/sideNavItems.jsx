33f8119c280b93e958cf45eb0aa5cc55c3377443
Nicholas Wease <skynet60@outlook.com>
2019-03-25 13:24:36 +0100

    Continued Work on setting up the side nav.

--- src/components/Header/SideNav/sideNavItems.jsx
+++ src/components/Header/SideNav/sideNavItems.jsx
@@ -4,12 +4,41 @@ import FontAwesome from 'react-fontawesome';
 import './sideNav.css'
 
 const SideNavItems = () => {
+
+    const items = [
+        {
+            icon: 'home',
+            text: 'HOME',
+            link: '/'
+        },
+        {
+            icon: 'file-text-o',
+            text: 'NEWS',
+            link: '/news'
+        },
+        {
+            icon: 'play',
+            text: 'VIDEOS',
+            link: '/videos'
+        },
+    ];
+
+    const showItems = () => {
+        return items.map((item, i) => {
+            return (
+                <div key={i} className={item.type}>
+                    <Link to={item.link}>
+                        <FontAwesome name={item.icon} />
+                        {item.text}
+                    </Link>
+                </div>
+            )
+        })
+    };
+
     return (
-        <div className='options'>
-            <Link to='/'>
-                <FontAwesome name='home' />
-                HOME
-            </Link>
+        <div>
+            {showItems()}
         </div>
     );
 };
